2160864c699161ed1905d0e2c771e65c5a4b5167
love.cosma <williamsmatthew333@gmail.com>
2021-10-12 13:29:31 -0600

    Added 3 components and file structure.

--- src/App.js
+++ src/App.js
@@ -1,3 +1,7 @@
+import {BrowserRouter as Router, Switch, Route} from "react-router"
+import Home from "./components/Home"
+import UploadPosts from "./components/UploadPosts"
+import Posts from "./containers/Posts"
 
 
 function App() {
